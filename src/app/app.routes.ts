c97aa8de48fa1b687328dce4d720708dfa08fcbc
Santiago Briceño <83045233+SantiagoBriceno@users.noreply.github.com>
2025-03-05 08:47:21 -0400

    feat: add new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,29 @@
 import { Routes } from '@angular/router'
 import { LandingPageComponent } from './views/landing/landing-page.component'
+import { AboutUsComponent } from './views/about-us/about-us.component'
+import { AdminComponent } from './views/admin/admin.component'
+import { ContactUsPageComponent } from './views/contact-us/contact-us-page.component'
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'home',
     component: LandingPageComponent
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
+  },
+  {
+    path: 'contact-us',
+    component: ContactUsPageComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
   }
 ]
