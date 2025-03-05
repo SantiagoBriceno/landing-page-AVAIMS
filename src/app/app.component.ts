3246e681a3f381bda0bb8f2fda253f704a7f7330
Santiago Briceño <83045233+SantiagoBriceno@users.noreply.github.com>
2025-03-05 10:43:30 -0400

    feat: change routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
-import { HeaderComponent } from './views/shared/header/header.component'
-import { FooterComponent } from './views/shared/footer/footer.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet
+    // HeaderComponent,
+    //  FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,35 @@
 import { Routes } from '@angular/router'
-import { LandingPageComponent } from './views/landing/landing-page.component'
-import { AboutUsComponent } from './views/about-us/about-us.component'
+import { LandingPageComponent } from './views/home/landing/landing-page.component'
+import { AboutUsComponent } from './views/home/about-us/about-us.component'
 import { AdminComponent } from './views/admin/admin.component'
-import { ContactUsPageComponent } from './views/contact-us/contact-us-page.component'
+import { ContactUsPageComponent } from './views/home/contact-us/contact-us-page.component'
+import { HomeComponent } from './views/home/home.component'
 
 export const routes: Routes = [
   {
     path: 'home',
-    component: LandingPageComponent
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        component: LandingPageComponent
+      },
+      {
+        path: 'about-us',
+        component: AboutUsComponent
+      },
+      {
+        path: 'contact-us',
+        component: ContactUsPageComponent
+      }
+    ]
   },
-  {
-    path: 'about-us',
-    component: AboutUsComponent
-  },
+
   {
     path: 'admin',
     component: AdminComponent
   },
-  {
-    path: 'contact-us',
-    component: ContactUsPageComponent
-  },
+
   {
     path: '',
     redirectTo: 'home',
