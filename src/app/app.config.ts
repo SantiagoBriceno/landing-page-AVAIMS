dd0b8b602c7c7b42674323e03206241aa120b200
Santiago Briceño <83045233+SantiagoBriceno@users.noreply.github.com>
2025-03-05 08:47:14 -0400

    feat: new app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core'
 import { provideRouter } from '@angular/router'
-
 import { routes } from './app.routes'
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser'
+import { provideHttpClient, withFetch } from '@angular/common/http'
+import { provideAnimations } from '@angular/platform-browser/animations'
+import localeEs from '@angular/common/locales/es'
+import { registerLocaleData } from '@angular/common'
+registerLocaleData(localeEs, 'es')
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' },
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimations(),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(
+      withFetch()
+    )]
 }
