073fac821a4eb610e3bce65ae82e262ea9217654
Santiago Briceño <83045233+SantiagoBriceno@users.noreply.github.com>
2025-03-25 19:38:59 -0400

    feat: routing

--- src/app/views/home/landing/components/last-notice/last-notice.component.ts
+++ src/app/views/home/landing/components/last-notice/last-notice.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core'
 import { MyCardComponent } from '../../../../shared/card/my-card.component'
 import { LastNoticeService } from './last-notice.service'
 import { Notice } from '../../../../../../types'
+import { RouterModule } from '@angular/router'
 
 @Component({
   selector: 'app-last-notice',
   templateUrl: './last-notice.component.html',
   styleUrls: ['./last-notice.component.scss'],
   standalone: true,
-  imports: [MyCardComponent]
+  imports: [MyCardComponent, RouterModule]
 })
 export class LastNoticeComponent implements OnInit {
   public notices: Notice[] = []
